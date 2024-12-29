313b9497d3fd2a1bcae361e9e712ff94fd0894e0
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-29 11:16:44 -0500

    add auth tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+    "testing"
+    "net/http"
+    "strings"
+)
+
+func TestGetAPIKey(t *testing.T) {
+    var headers http.Header
+    var result string
+    var err error
+
+    headers = http.Header {}
+    headers.Add("Authorization", "ApiKey test")
+    result, err = GetAPIKey(headers)
+    if err != nil {
+        t.Errorf("errored but shouldn't have: %v\n", err.Error())
+        return
+    }
+    if result != "test" {
+        t.Errorf(`incorrect api key: expected "test" but got "%v"\n`, result)
+        return
+    }
+
+    headers = http.Header {}
+    result, err = GetAPIKey(headers)
+    if err == nil {
+        t.Error("did not error but should have")
+        return
+    }
+    if err != ErrNoAuthHeaderIncluded {
+        t.Error("errored but did not return the correct error type, expected ErrNoAuthHeaderIncluded")
+        return
+    }
+
+    headers = http.Header {}
+    headers.Add("Authorization", "Bearer test")
+    result, err = GetAPIKey(headers)
+    if err == nil {
+        t.Error("did not error but should have")
+        return
+    }
+    if !strings.Contains(err.Error(), "malformed") {
+        t.Errorf(`errored but got unexpected message: "%v" should have contained "malformed"\n`, err.Error())
+        return
+    }
+}
